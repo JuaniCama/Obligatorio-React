3b452d9e69417938efb4b18801d482711c9cdcba
Diego Vázquez <dvazquezlagreca@gmail.com>
2024-09-23 19:39:18 -0300

    Home+Profile
    Páginas base para home y profile

---
+++ Fakestagram/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to the Fakestagram Home page! 😎</p>
+    </div>
+  );
+}
+
+export default Home;
