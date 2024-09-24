82aab9a7b71a513ebc6d12f05171c959eb98dad6
JuaniCama <juanignaciocama@gmail.com>
2024-09-24 00:04:00 -0300

    Ordenado de archivos, debe tirarse el 'npm run dev' desde la carpeta 'Fakestagram'

---
+++ Fakestagram/src/web/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from '@web/components/Home.jsx';
+import Profile from '@web/components/Profile.jsx';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
