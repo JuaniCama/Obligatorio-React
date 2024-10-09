cd0825d84f4421506d845c345f9029210d5d6397
Diego Vázquez <dvazquezlagreca@gmail.com>
2024-10-09 20:56:55 -0300

    Preparando para pull request

--- Fakestagram/web/src/web/components/Home.jsx
+++ Fakestagram/web/src/web/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Post from './Post'; // Importa el componente Post
+import './Home.css'; // Archivo CSS que contiene los estilos
+
 
 function Home() {
   return (
--- Fakestagram/web/src/web/components/Post.jsx
+++ Fakestagram/web/src/web/components/Post.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
+import './Post.css';
+
 
 function Post({ username, profileImageUrl, postTime, imageUrl, description, profileView = false }) {
   return (
+    // <div className={`box ${profileView ? 'profile-view' : 'feed-view'}`}>
     <div className={`box ${profileView ? 'profile-view' : 'feed-view'}`}>
-      {/* Solo muestra el header si no está en la vista de perfil */}
-      {!profileView && (
-        <article className="media">
-          <figure className="media-left">
-            <p className="image is-64x64">
-              <img src={profileImageUrl} alt={`${username}'s profile`} />
-            </p>
-          </figure>
-          <div className="media-content">
-            <div className="content">
-              <p>
-                <strong>{username}</strong> <small>• {postTime}</small>
-                <br />
-                {description}
-              </p>
-            </div>
-          </div>
-        </article>
-      )}
+
       {/* Imagen del post */}
       <div className={`image ${profileView ? 'is-square' : 'is-4by3'}`}>
-        <img src={imageUrl} alt="Post" />
+          <img src={imageUrl} alt="Post" />
       </div>
+
+      {/* Solo muestra el header si no está en la vista de perfil */ }
+      {
+        !profileView && (
+          <article className="media">
+            <figure className="media-left">
+              <p className="image is-64x64">
+                <img src={profileImageUrl} alt={`${username}'s profile`} />
+              </p>
+            </figure>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>{username}</strong> <small>• {postTime}</small>
+                  <br />
+                  {description}
+                </p>
+              </div>
+            </div>
+          </article>
+        )}
     </div>
   );
 }
