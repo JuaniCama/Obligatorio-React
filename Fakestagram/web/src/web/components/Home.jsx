eb3b33b6d1efe90883ab05df38a6a35eddba3aff
ignrdz2 <ignrdz55@gmail.com>
2024-10-09 19:39:10 -0300

    componente post adaptable

--- Fakestagram/web/src/web/components/Home.jsx
+++ Fakestagram/web/src/web/components/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import Post from './Post'; // Importa el componente Post
 
 function Home() {
   return (
     <div>
       <h2>Home!!</h2>
       <p>Welcome to the Fakestagram Home page! 😎</p>
+
+      {/* Ejemplo de un Post en el feed */}
+      <div>
+        <Post 
+          username="usuario123"
+          profileImageUrl="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR-rsc7p9uXmx-i2PbvpYl34uQQFrlJ6VcwHnzq7-yLEuaQpGLE75otAhKvvXyBWZXeUWoyVjhQUu965l0t6DMZ6dDnPDRX1EofAcJFYg"
+          postTime="5 min"
+          imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/cnne-412093-170522202550-02-versova-beach-exlarge-169.jpg?c=original"
+          description="Disfrutando el día en la playa! 🌞"
+          profileView={false} // Indica que se muestra en el feed
+        />
+        <Post 
+        username="usuario123"
+        profileImageUrl="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR-rsc7p9uXmx-i2PbvpYl34uQQFrlJ6VcwHnzq7-yLEuaQpGLE75otAhKvvXyBWZXeUWoyVjhQUu965l0t6DMZ6dDnPDRX1EofAcJFYg"
+        postTime="5 min"
+        imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/cnne-412093-170522202550-02-versova-beach-exlarge-169.jpg?c=original"
+        description="Disfrutando el día en la playa! 🌞"
+        profileView={false} // Indica que se muestra en el feed
+      />
+      </div>
     </div>
   );
 }
--- Fakestagram/web/src/web/components/Post.jsx
+++ Fakestagram/web/src/web/components/Post.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
-function Post() {
+function Post({ username, profileImageUrl, postTime, imageUrl, description, profileView = false }) {
   return (
-    <div>
-      <h2> Foto </h2>
-
+    <div className={`box ${profileView ? 'profile-view' : 'feed-view'}`}>
+      {/* Solo muestra el header si no está en la vista de perfil */}
+      {!profileView && (
+        <article className="media">
+          <figure className="media-left">
+            <p className="image is-64x64">
+              <img src={profileImageUrl} alt={`${username}'s profile`} />
+            </p>
+          </figure>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{username}</strong> <small>• {postTime}</small>
+                <br />
+                {description}
+              </p>
+            </div>
+          </div>
+        </article>
+      )}
+      {/* Imagen del post */}
+      <div className={`image ${profileView ? 'is-square' : 'is-4by3'}`}>
+        <img src={imageUrl} alt="Post" />
+      </div>
     </div>
   );
 }
