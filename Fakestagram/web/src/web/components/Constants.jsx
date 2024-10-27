af5400acf191d64046b6147eaf4d0c317517a9cc
Álvaro <alvarobaladan@gmail.com>
2024-10-26 23:03:24 -0300

    Obtiene y muestra los posts del feed (Primera Versión - WIP). Agrega ícono web.

---
+++ Fakestagram/web/src/web/components/Constants.jsx
@@ -0,0 +1,2 @@
+export const API_BASE_URL = 'http://localhost:3001';
+export const FEED_ENDPOINT = `${API_BASE_URL}/api/posts/feed`;
--- Fakestagram/web/src/web/components/Home.jsx
+++ Fakestagram/web/src/web/components/Home.jsx
@@ -1,39 +1,51 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Post from './Post'; // Importa el componente Post
 import Notifications from './Notifications'; // Importa el componente Notification
 import './Home.css'; // Archivo CSS que contiene los estilos
+import { FEED_ENDPOINT } from './Constants';
+
+const fetchPosts = async () => {
+  try {
+    const token = localStorage.token;
+    const response = await axios.get(`${FEED_ENDPOINT}`, {
+      headers: { "Authorization": `Bearer ${token}` }
+    }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => async () => {
+    const data = await fetchPosts();
+    setPosts(data);
+  }, [])
+
   return (
     <div className="content">
       <div className="feed">
         <h2>Acá habria historias si me pagaran</h2>
 
-        {/* Ejemplo de un Post en el feed */}
-        <Post 
-          username="walter_blanco"
-          profileImageUrl="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR-rsc7p9uXmx-i2PbvpYl34uQQFrlJ6VcwHnzq7-yLEuaQpGLE75otAhKvvXyBWZXeUWoyVjhQUu965l0t6DMZ6dDnPDRX1EofAcJFYg"
-          postTime="5 min"
-          imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/cnne-412093-170522202550-02-versova-beach-exlarge-169.jpg?c=original"
-          description="Disfrutando el día en la playa! 🌞"
-          profileView={false} // Indica que se muestra en el feed
-        />
-        <Post 
-          username="luissuarez9"
-          profileImageUrl="https://pbs.twimg.com/profile_images/1658477624048615427/hnP_Zpy5_400x400.jpg"
-          postTime="15 min"
-          imageUrl="https://pbs.twimg.com/profile_images/1658477624048615427/hnP_Zpy5_400x400.jpg"
-          description="#NuevaFotoDePerfil"
-          profileView={false} // Indica que se muestra en el feed
-        />
-        <Post 
-          username="oficialcap"
-          profileImageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIw9PxZ5P-pLcFS3O0iqDixKQpxvXsZ50gOA&s"
-          postTime="2 horas"
-          imageUrl="https://pbs.twimg.com/media/Fb_BkghWYAM-uVS.jpg"
-          description="asdsdadsa"
-          profileView={false} // Indica que se muestra en el feed
-        />
+        <div>
+          {posts.map((post) =>
+            <Post
+              key={post.id}
+              username={post.user.username}
+              profileImageUrl={post.user.profileImageUrl}
+              postTime={post.createdAt}
+              imageUrl={post.imageUrl}
+              description={post.caption}
+              profileView={false}
+            />
+          )}
+        </div>
       </div>
 
       <div className="notifications">
